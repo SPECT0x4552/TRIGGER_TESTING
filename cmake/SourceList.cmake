a0b19e5c0fcf90eb25081f3bd30e69a6a3706591
Karl Marten Jaaska <valdisreturns@proton.me>
2024-10-17 00:34:32 +0300

    changes

---
+++ cmake/SourceList.cmake
@@ -0,0 +1,26 @@
+set(source_list ${source_list}
+${PROJ_PATH}/Core/Src/main.c
+${PROJ_PATH}/Core/Src/stm32g4xx_hal_msp.c
+${PROJ_PATH}/Core/Src/stm32g4xx_it.c
+${PROJ_PATH}/Core/Src/syscalls.c
+${PROJ_PATH}/Core/Src/sysmem.c
+${PROJ_PATH}/Core/Src/system_stm32g4xx.c
+${PROJ_PATH}/Core/Startup/startup_stm32g431rbtx.s
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c
+${PROJ_PATH}/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c
+
+
+
+)
